88441a3c9059e567b0d9c576c98666379375ae8b
atish kumar <atishr334@gmail.com>
2026-02-28 16:53:45 +0530

    local skills icons map and skills aliases (fuzzy match)

---
+++ src/utils/skillIcons.js
@@ -0,0 +1,112 @@
+// 🔹 LOCAL ICONS (PRIMARY)
+export const LOCAL_SKILL_ICON_MAP = {
+  react: [
+    '/react.svg',
+    '/reactjs.webp',
+    '/icons8-react-office-m/icons8-react-16.png',
+    '/icons8-react-office-m/icons8-react-30.png',
+    '/icons8-react-office-m/icons8-react-40.png',
+    '/icons8-react-office-m/icons8-react-80.png',
+    '/icons8-react-ios-17-glyph(1)/icons8-react-30.svg',
+    '/icons8-react-ios-17-glyph(1)/icons8-react-60.svg',
+    '/icons8-react-ios-17-glyph(1)/icons8-react-90.svg',
+    '/icons8-react-ios-17-glyph(1)/icons8-react-120.svg',
+    '/icons8-react-ios-17-glyph(1)/icons8-react-240.svg',
+    '/icons8-react-ios-17-glyph/icons8-react-30.png',
+    '/icons8-react-ios-17-glyph/icons8-react-60.png',
+    '/icons8-react-ios-17-glyph/icons8-react-90.png',
+    '/icons8-react-ios-17-glyph/icons8-react-120.png',
+    '/icons8-react-ios-17-glyph/icons8-react-240.png',
+    '/icons8-react-ios-17-glyph/icons8-react-480.png',
+    
+  ],
+  cloudinary: [
+    '/cloudinary.svg'
+  ],
+  css: [
+    '/css.webp'
+  ],
+  docker: [
+    '/docker.webp'
+  ],
+  express: [
+    '/express.webp'
+  ],
+  figma: [
+    '/figma.webp'
+  ],
+  firebase: [
+    '/firebase.webp'
+  ],
+  html: [
+    '/html.webp'
+  ],
+  mongodb: [
+    '/mongodb.webp'
+  ],
+  node: [
+    '/nodejs.webp'
+  ],
+  python: [
+    '/Python.webp'
+  ],
+  redux: [
+    '/redux.webp'
+  ],
+  tailwind: [
+    '/tailwind.webp'
+  ],
+  three: [
+    '/threejs.svg'
+  ],
+  typescript: [
+    '/typescript.webp'
+  ],
+  git: [
+    '/git.webp',
+    '/githubTech.webp'
+  ],
+  javascript: [
+    '/javascript.webp'
+  ],
+  excel: [
+    
+  ],
+  appwrite:[
+    '/appwrite.webp'
+  ],
+  axios:[
+    '/axios.webp'
+  ],
+  vite:[
+    '/vite.svg'
+  ],
+
+
+}
+
+
+// 🔹 SKILL ALIASES (FUZZY MATCH)
+export const SKILL_ALIASES = {
+  javascript: ['js', 'javascript', 'java script','script'],
+  react: ['react', 'reactjs', 'react.js'],
+  excel: ['excel', 'ms excel'],
+  appwrite:['appwrite','tool'],
+  axios:['axios','tool'],
+  cloudinary:['cloudinary','tool'],
+  css:['css','style','styles'],
+  docker:['docker','tools','tool'],
+  express:['ex','express','expressjs','express.js','backend'],
+  figma:['fg','figma','style','styles','design','ux','ui'],
+  firebase:['fb','firebase','tools','tool','data'],
+  git:['git','github','tools','tool',],
+  html:['html','structure',],
+  node:['node','nodejs','backend','node.js'],
+  mongodb:['mg','mongodb','database','data','mongo'],
+  python:['py','python','backend',],
+  redux:['redux','redux.js','reduxjs'],
+  tailwind:['tailwind','tailwind.css','tailwindcss','style','styles'],
+  three:['three','three.js','threejs'],
+  typescript:['ts','typescript','type script','script'],
+  vite:['vite','vite.js','tool','tools','react'],
+}
