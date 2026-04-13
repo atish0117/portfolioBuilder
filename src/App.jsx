9454d95bf6e7f8f652b5cae2ea9a3757fa8d16c9
atish kumar <atishr334@gmail.com>
2026-04-14 01:34:35 +0530

    update all frontend routes and make a more effician and more capble and effective routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,75 @@
-import React, { useEffect } from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { motion, AnimatePresence } from 'framer-motion'
-import { checkAuth } from './store/slices/authSlice'
-import { ThemeProvider } from './contexts/ThemeContext'
-import ErrorBoundary from './components/ui/ErrorBoundary'
-import Navbar from './components/layout/Navbar'
-import LoadingSpinner from './components/ui/LoadingSpinner'
-import Login from './pages/auth/Login'
-import Register from './pages/auth/Register'
-import AuthCallback from './pages/auth/AuthCallback'
-import ForgotPassword from './pages/auth/ForgotPassword'  
-import ResetPassword from './pages/auth/ResetPassword' 
-import AdminDashboard from './pages/admin/AdminDashboard'
-import Dashboard from './pages/Dashboard'
-import Portfolio from './pages/Portfolio'
-import Home from './pages/Home' 
-import AuroraStudioTemplate from "./components/portfolio/templates/AuroraStudioTemplate"
-import { useLocation } from 'react-router-dom'
-
-
-
+import React, { useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { motion, AnimatePresence } from "framer-motion";
+import { checkAuth } from "./store/slices/authSlice";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import ErrorBoundary from "./components/ui/ErrorBoundary";
+import Navbar from "./components/layout/Navbar";
+import LoadingSpinner from "./components/ui/LoadingSpinner";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import AuthCallback from "./pages/auth/AuthCallback";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+import ResetPassword from "./pages/auth/ResetPassword";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import Dashboard from "./pages/Dashboard";
+import Portfolio from "./pages/Portfolio";
+import Home from "./pages/Home";
+import AuroraStudioTemplate from "./components/portfolio/templates/AuroraStudioTemplate";
+import { useLocation } from "react-router-dom";
+import api from './services/api'
 function App() {
-  const dispatch = useDispatch()
-   const location = useLocation() // 👈 gets current path
-  const { isAuthenticated, loading,user } = useSelector((state) => state.auth)
-  console.log('Auth state:', isAuthenticated, loading ,user)
+  const dispatch = useDispatch();
+  const location = useLocation(); // 👈 gets current path
+  const { isAuthenticated, loading, user } = useSelector((state) => state.auth);
+  console.log("Auth state:", isAuthenticated, loading, user);
 
   useEffect(() => {
-    dispatch(checkAuth())
-  }, [dispatch])
+    let isRefreshing=false
+    const initAuth=async()=>{
+      try {
+        // refresh toker 
+        await api.post("/auth/refresh")
+        // after that check auth
+        dispatch(checkAuth());
+
+      } catch (error) {
+        console.log(error,"Not logged in or refresh failed");
+      }
+    }
+    initAuth()
+
+    const interval= setInterval(async()=>{
+        if(isRefreshing) return
+              isRefreshing=true
+                try {
+                  await api.post("/auth/refresh")
+                } catch (error) {
+                  console.log(error, "silent Refresh failed")
+                } finally{
+                  isRefreshing= false
+                }
+
+    }, 10*60*1000)
+
+    return()=>{clearInterval(interval)}
+  }, [dispatch]);
 
   if (loading) {
-    return <LoadingSpinner />
+    return <LoadingSpinner />;
   }
   // 👇 Define routes where navbar should not appear
 
-const shouldHideNavbar = /^\/(?!dashboard|login|register$)[^/]+$/.test(location.pathname)
-
+  const shouldHideNavbar = /^\/(?!dashboard|login|register$)[^/]+$/.test(
+    location.pathname,
+  );
 
   return (
     <ErrorBoundary>
       <ThemeProvider>
         <div className="min-h-screen bg-white dark:bg-dark-900 transition-colors duration-300">
-             {/* Conditionally render Navbar */}
+          {/* Conditionally render Navbar */}
           {!shouldHideNavbar && <Navbar />}
 
           <AnimatePresence mode="wait">
@@ -51,54 +77,103 @@ const shouldHideNavbar = /^\/(?!dashboard|login|register$)[^/]+$/.test(location.
               <Route path="/" element={<Home />} />
               <Route path="/demo" element={<AuroraStudioTemplate />} />
 
-              <Route 
-                path="/login" 
+              <Route
+                path="/login"
                 element={
-                  !isAuthenticated ? <Login /> : <Navigate to="/dashboard" replace />
-                } 
+                  !isAuthenticated ? (
+                    <Login />
+                  ) : (
+                    <Navigate
+                      to={
+                        user?.role?.toLowerCase() === "admin"
+                          ? "/admin"
+                          : "/dashboard"
+                      }
+                      replace
+                    />
+                  )
+                }
               />
-              <Route 
-                path="/register" 
+
+              <Route
+                path="/register"
                 element={
-                  !isAuthenticated ? <Register /> : <Navigate to="/dashboard" replace />
-                } 
+                  !isAuthenticated ? (
+                    <Register />
+                  ) : (
+                    <Navigate
+                      to={
+                        user?.role?.toLowerCase() === "admin"
+                          ? "/admin"
+                          : "/dashboard"
+                      }
+                      replace
+                    />
+                  )
+                }
               />
-              <Route 
-                path="/forgot-password" 
+              <Route
+                path="/forgot-password"
                 element={
-                  !isAuthenticated ? <ForgotPassword /> : <Navigate to="/dashboard" replace />
-                } 
+                  !isAuthenticated ? (
+                    <ForgotPassword />
+                  ) : (
+                    <Navigate
+                      to={
+                        user?.role?.toLowerCase() === "admin"
+                          ? "/admin"
+                          : "/dashboard"
+                      }
+                      replace
+                    />
+                  )
+                }
               />
-              <Route 
-                path="/reset-password" 
+              <Route
+                path="/reset-password"
                 element={
-                  !isAuthenticated ? <ResetPassword /> : <Navigate to="/dashboard" replace />
-                } 
+                  !isAuthenticated ? (
+                    <ResetPassword />
+                  ) : (
+                    <Navigate
+                      to={
+                        user?.role?.toLowerCase() === "admin"
+                          ? "/admin"
+                          : "/dashboard"
+                      }
+                      replace
+                    />
+                  )
+                }
               />
 
               <Route path="/auth/callback" element={<AuthCallback />} />
 
-              <Route 
-
+              <Route
                 path="/dashboard"
-
                 element={
-                  isAuthenticated ? <Dashboard /> : <Navigate to="/login" replace />
-                } 
+                  !isAuthenticated ? (
+                    <Navigate to="/login" replace />
+                  ) : user?.role?.toLowerCase() === "admin" ? (
+                    <Navigate to="/admin" replace />
+                  ) : (
+                    <Dashboard />
+                  )
+                }
               />
 
-                <Route 
-                path="/admin" 
+              <Route
+                path="/admin"
                 element={
-                  isAuthenticated ? <AdminDashboard /> : <Navigate to="/login" replace />
-                } 
+                  !isAuthenticated ? (
+                    <Navigate to="/login" replace />
+                  ) : user?.role?.toLowerCase() === "admin" ? (
+                    <AdminDashboard />
+                  ) : (
+                    <Navigate to="/dashboard" replace />
+                  )
+                }
               />
-
-
-
-                
-
-
 
               <Route path="/:username" element={<Portfolio />} />
             </Routes>
@@ -107,7 +182,7 @@ const shouldHideNavbar = /^\/(?!dashboard|login|register$)[^/]+$/.test(location.
         {/* <DebugPanel /> */}
       </ThemeProvider>
     </ErrorBoundary>
-  )
+  );
 }
 
-export default App
+export default App;
