19594837acf426bb12c5aeff798a8a35a681d658
atish kumar <atishr334@gmail.com>
2026-04-14 00:19:17 +0530

    fully update file upload middleware for stoge file image and resume in cloudinary and create url and these url store in DB

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,29 +1,40 @@
 import multer from "multer"
 import path from "path"
+import fs from "fs"
 
-// temp storage (cloudinary me upload hoga )
+// temp storage
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/")
+    const dir = "uploads/"
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir)
+    }
+    cb(null, dir)
   },
   filename: (req, file, cb) => {
-    const unique = Date.now() + path.extname(file.originalname)
-    cb(null, unique)
+    const unique = Date.now() + "-" + Math.round(Math.random() * 1e9)
+    cb(null, unique + path.extname(file.originalname))
   }
 })
 
+// UPDATED FILTER (image + pdf support)
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith("image")) {
+  if (
+    file.mimetype.startsWith("image") || // profile image
+    file.mimetype === "application/pdf" // resume
+  ) {
     cb(null, true)
   } else {
-    cb(new Error("Only images allowed"), false)
+    cb(new Error("Only images & PDF allowed"), false)
   }
 }
 
 const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 2 * 1024 * 1024 } // 2mb
+  limits: {
+    fileSize: 5 * 1024 * 1024 // 5MB
+  }
 })
 
 export default upload
